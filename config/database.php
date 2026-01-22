3bd5f8997a6fb7f874e7cf6075219dccda8d8c4a
yuri-kana <153376547+yuri-kana@users.noreply.github.com>
2026-01-22 13:39:45 +0800

    Update database options for SSL configuration

--- config/database.php
+++ config/database.php
@@ -58,9 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => [
+                PDO::MYSQL_ATTR_SSL_CA => base_path('certs/ca-certificates.crt'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ],
         ],
 
         'pgsql' => [
